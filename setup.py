2b6f7cd35c26655f105b99f630efe081f5a18dce
Gaurav Gupta <gdgupta11@hotmail.com>
2024-04-19 17:37:45 +0530

    Fixing the missing templates folder on deployment of package file via pip

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # A python setup file for creating python package
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='flaskapp',
-    version='2.0',
-    packages=find_packages(),
+    packages=find_namespace_packages(),
+    include_package_data=True,
     install_requires=[
         'Flask',
         'pymongo'
